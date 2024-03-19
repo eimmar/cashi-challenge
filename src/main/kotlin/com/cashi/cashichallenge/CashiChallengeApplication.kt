3dcefc6bcfcef4452188a59c6a035164c59b1ee6
Eimantas Markevičius <eimantas.markevicius@nfq.lt>
2024-03-19 09:07:52 +0200

    initial commit and project setup

---
+++ src/main/kotlin/com/cashi/cashichallenge/CashiChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.cashi.cashichallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CashiChallengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CashiChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/com/cashi/cashichallenge/CashiChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cashi.cashichallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CashiChallengeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
